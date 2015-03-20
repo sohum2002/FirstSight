b6edfc74fc89ae2edea61aef9d88cd976c8e7c3f
sohum2002 <sohumsachdev@Sohums-MacBook-Pro.local>
2015-03-20 15:16:21 +0700

    Loose ends tied up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,13 +9,22 @@ class ApplicationController < ActionController::Base
 	# redirect_back_or root_path
 	# end
 	 
-	def redirect_back_or(path)
-	  redirect_to request.referer || path
-	end
+	# def redirect_back_or(path)
+	#   redirect_to request.referer || path
+	# end
 
   def configure_permitted_parameters
 	  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :first_name, :last_name, :about, :occupation, :field, :orientation, :interest, :physical, :free_time, :image, :gender, :birthday)}
 	end
   
+  def after_sign_in_path_for(user)
+  	users_path
+  end
+
+  def after_sign_up_path_for(user)
+  	user_url(user)
+  end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  devise_for :users
 
   root "static_pages#home"
 
