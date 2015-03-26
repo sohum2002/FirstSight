4374180000f1bd4187f104d3921fc669c07b3792
sohum2002 <sohumsachdev@Sohums-MacBook-Pro.local>
2015-03-26 23:27:51 +0700

    Found new problem

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,14 @@
   class MessagesController < ApplicationController
-    # before_action :authenticate_user!
+    before_action :authenticate_user!
  
   def new
-    @chosen_recipient = User.find_by(id: params[:to].to_i) if params[:to]
   end
  
   def create
     recipients = User.where(id: params['recipients'])
     conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
-    flash[:success] = "Message has been sent!"
+    flash[:success] = "Your message has been sent!"
+    #pass the recipients, body of message and subject of message
     redirect_to conversation_path(conversation)
   end
 end
