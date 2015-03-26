50c25d5907ba0bbf9fe87fc0fca4f4149705b3bd
sohum2002 <sohumsachdev@Sohums-MacBook-Pro.local>
2015-03-26 23:22:15 +0700

    Bugs nearly all fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   
-  before_filter :configure_permitted_parameters, :only => [:create]
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   	
 	protect_from_forgery with: :exception
   before_action :authenticate_user!,:except => [:home]  
@@ -14,8 +14,8 @@ class ApplicationController < ActionController::Base
 	#   redirect_to request.referer || path
 	# end
 
-  def configure_permitted_parameters
-	  # devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :first_name, :last_name, :about, :occupation, :field, :orientation, :interest, :physical, :free_time, :image, :gender, :birthday)}
+  	def configure_permitted_parameters
+	  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :first_name, :last_name, :about, :occupation, :field, :orientation, :interest, :physical, :free_time, :image, :gender, :birthday)}
 	end
   
   def after_sign_in_path_for(user)
