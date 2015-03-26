49bfd964e86bc6840813c3bf6cb3bd2644bcf642
sohum2002 <sohumsachdev@Sohums-MacBook-Pro.local>
2015-03-27 00:45:37 +0700

    ll

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   
   before_filter :configure_permitted_parameters, if: :devise_controller?
-<<<<<<< HEAD
-  	
-=======
->>>>>>> old-state
 	protect_from_forgery with: :exception
   before_action :authenticate_user!,:except => [:home]  
 
