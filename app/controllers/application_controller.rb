082e8453e1048bed63bd1a1e1832a4296f7655e2
sohum2002 <sohumsachdev@Sohums-MacBook-Pro.local>
2015-03-27 00:37:47 +0700

    Whole project completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   
-  before_filter :configure_permitted_parameters, :only => [:create]
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 	protect_from_forgery with: :exception
   before_action :authenticate_user!,:except => [:home]  
 
